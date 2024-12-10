e6a4cfd05f23c303f3d7e96bc2218442300d6d72
syed quaid <zaiyanhussain86@gmail.com>
2024-12-10 18:46:20 +0530

    done

---
+++ 1.Type Annotations.ts
@@ -0,0 +1,14 @@
+// Write a function that takes two numbers as arguments and returns their sum. Ensure the function parameters and return type are annotated.
+
+// Complete Below Function 
+
+// console.log(add(2, 3)); // Output: 5
+
+function add(a:number,b:number):number{
+    return a+b;
+}
+
+
+console.log(add(2,3))
+
+export{}
